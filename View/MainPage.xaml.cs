11f606b6a160c1a367ce07ba47ff05382d9925c7
jsmit124 <jsmit124@my.westga.edu>
2019-11-18 18:56:03 -0500

    Add project files.

---
+++ View/MainPage.xaml.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Graphics.Imaging;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace GroupNStegafy.View
+{
+    /// <summary>
+    ///     The main page used as an entry point for the program
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        #region Data members
+
+        private double dpiX;
+        private double dpiY;
+        private WriteableBitmap modifiedImage;
+
+        #endregion
+
+        #region Constructors
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.modifiedImage = null;
+            this.dpiX = 0;
+            this.dpiY = 0;
+        }
+
+        #endregion
+
+        #region Methods
+
+        private async void openButton_Click(object sender, RoutedEventArgs e)
+        {
+            var sourceImageFile = await this.selectSourceImageFile();
+            var copyBitmapImage = await this.MakeACopyOfTheFileToWorkOn(sourceImageFile);
+
+            using (var fileStream = await sourceImageFile.OpenAsync(FileAccessMode.Read))
+            {
+                var decoder = await BitmapDecoder.CreateAsync(fileStream);
+                var transform = new BitmapTransform {
+                    ScaledWidth = Convert.ToUInt32(copyBitmapImage.PixelWidth),
+                    ScaledHeight = Convert.ToUInt32(copyBitmapImage.PixelHeight)
+                };
+
+                this.dpiX = decoder.DpiX;
+                this.dpiY = decoder.DpiY;
+
+                var pixelData = await decoder.GetPixelDataAsync(
+                    BitmapPixelFormat.Bgra8,
+                    BitmapAlphaMode.Straight,
+                    transform,
+                    ExifOrientationMode.IgnoreExifOrientation,
+                    ColorManagementMode.DoNotColorManage
+                );
+
+                var sourcePixels = pixelData.DetachPixelData();
+
+                this.giveImageRedTint(sourcePixels, decoder.PixelWidth, decoder.PixelHeight);
+
+                this.modifiedImage = new WriteableBitmap((int) decoder.PixelWidth, (int) decoder.PixelHeight);
+                using (var writeStream = this.modifiedImage.PixelBuffer.AsStream())
+                {
+                    await writeStream.WriteAsync(sourcePixels, 0, sourcePixels.Length);
+                    this.imageDisplay.Source = this.modifiedImage;
+                }
+            }
+        }
+
+        private void giveImageRedTint(byte[] sourcePixels, uint imageWidth, uint imageHeight)
+        {
+            for (var i = 0; i < imageHeight; i++)
+            {
+                for (var j = 0; j < imageWidth; j++)
+                {
+                    var pixelColor = this.GetPixelBgra8(sourcePixels, i, j, imageWidth, imageHeight);
+
+                    pixelColor.R = 255;
+
+                    this.SetPixelBgra8(sourcePixels, i, j, pixelColor, imageWidth, imageHeight);
+                }
+            }
+        }
+
+        private async Task<StorageFile> selectSourceImageFile()
+        {
+            var openPicker = new FileOpenPicker {
+                ViewMode = PickerViewMode.Thumbnail,
+                SuggestedStartLocation = PickerLocationId.PicturesLibrary
+            };
+            openPicker.FileTypeFilter.Add(".jpg");
+            openPicker.FileTypeFilter.Add(".png");
+            openPicker.FileTypeFilter.Add(".bmp");
+
+            var file = await openPicker.PickSingleFileAsync();
+
+            return file;
+        }
+
+        private async Task<BitmapImage> MakeACopyOfTheFileToWorkOn(StorageFile imageFile)
+        {
+            IRandomAccessStream inputStream = await imageFile.OpenReadAsync();
+            var newImage = new BitmapImage();
+            newImage.SetSource(inputStream);
+            return newImage;
+        }
+
+        /// <summary>
+        ///     Gets the pixel bgra8 color from the current pixel.
+        /// </summary>
+        /// <param name="pixels">The pixels.</param>
+        /// <param name="x">The x.</param>
+        /// <param name="y">The y.</param>
+        /// <param name="width">The width.</param>
+        /// <param name="height">The height.</param>
+        /// <returns>The color of the current pixel</returns>
+        public Color GetPixelBgra8(byte[] pixels, int x, int y, uint width, uint height)
+        {
+            var offset = (x * (int) width + y) * 4;
+            var r = pixels[offset + 2];
+            var g = pixels[offset + 1];
+            var b = pixels[offset + 0];
+            return Color.FromArgb(0, r, g, b);
+        }
+
+        /// <summary>
+        ///     Sets the pixel bgra8 color to the current pixel.
+        /// </summary>
+        /// <param name="pixels">The pixels.</param>
+        /// <param name="x">The x.</param>
+        /// <param name="y">The y.</param>
+        /// <param name="color">The color.</param>
+        /// <param name="width">The width.</param>
+        /// <param name="height">The height.</param>
+        public void SetPixelBgra8(byte[] pixels, int x, int y, Color color, uint width, uint height)
+        {
+            var offset = (x * (int) width + y) * 4;
+            pixels[offset + 2] = color.R;
+            pixels[offset + 1] = color.G;
+            pixels[offset + 0] = color.B;
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.saveWritableBitmap();
+        }
+
+        private async void saveWritableBitmap()
+        {
+            var fileSavePicker = new FileSavePicker {
+                SuggestedStartLocation = PickerLocationId.PicturesLibrary,
+                SuggestedFileName = "image"
+            };
+            fileSavePicker.FileTypeChoices.Add("PNG files", new List<string> {".png"});
+            var saveFile = await fileSavePicker.PickSaveFileAsync();
+
+            if (saveFile != null)
+            {
+                var stream = await saveFile.OpenAsync(FileAccessMode.ReadWrite);
+                var encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.PngEncoderId, stream);
+
+                var pixelStream = this.modifiedImage.PixelBuffer.AsStream();
+                var pixels = new byte[pixelStream.Length];
+                await pixelStream.ReadAsync(pixels, 0, pixels.Length);
+
+                encoder.SetPixelData(BitmapPixelFormat.Bgra8, BitmapAlphaMode.Ignore,
+                    (uint) this.modifiedImage.PixelWidth,
+                    (uint) this.modifiedImage.PixelHeight, this.dpiX, this.dpiY, pixels);
+                await encoder.FlushAsync();
+
+                stream.Dispose();
+            }
+        }
+
+        #endregion
+    }
+}
