77a54146ea3b858843bdcebdb07d9adc303532d9
Daniel Jeselnik <dannyjeselnik@gmail.com>
2019-11-19 20:02:27 -0500

    Adjusting size for ExtractMessage

--- View/ExtractMessagePage.xaml.cs
+++ View/ExtractMessagePage.xaml.cs
@@ -1,4 +1,9 @@
 ﻿
+using Windows.Foundation;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml.Media.Imaging;
+using GroupNStegafy.IO;
+
 namespace GroupNStegafy.View
 {
     /// <summary>
@@ -6,9 +11,33 @@ namespace GroupNStegafy.View
     /// </summary>
     public sealed partial class ExtractMessagePage
     {
+
+        private double dpiX;
+        private double dpiY;
+        private WriteableBitmap modifiedImage;
+        private readonly FileWriter fileWriter;
+        private readonly FileReader fileReader;
+
+
         public ExtractMessagePage()
         {
             this.InitializeComponent();
+
+            var applicationWidth = 1600;
+            var applicationHeight = 900;
+
+            ApplicationView.PreferredLaunchViewSize = new Size
+                { Width = applicationWidth, Height = applicationHeight };
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+            ApplicationView.GetForCurrentView()
+                           .SetPreferredMinSize(new Size(applicationWidth, applicationHeight));
+
+            this.modifiedImage = null;
+            this.dpiX = 0;
+            this.dpiY = 0;
+
+            this.fileWriter = new FileWriter();
+            this.fileReader = new FileReader();
         }
     }
 }
