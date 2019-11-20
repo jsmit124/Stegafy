c45a2d2ecde84021b98ca436f9b6a25de574e824
jsmit124 <jsmit124@my.westga.edu>
2019-11-19 19:27:53 -0500

    Modified home page and add embed page

---
+++ View/EmbedMessagePage.xaml.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Imaging;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+using GroupNStegafy.IO;
+using Windows.UI;
+using Windows.UI.ViewManagement;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace GroupNStegafy.View
+{
+    /// <summary>
+    ///     The main page used as an entry point for the program
+    /// </summary>
+    public sealed partial class EmbedMessagePage : Page
+    {
+        #region Data members
+
+        private double dpiX;
+        private double dpiY;
+        private WriteableBitmap modifiedImage;
+        private readonly FileWriter fileWriter;
+        private readonly FileReader fileReader;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="MainPage" /> class.
+        /// </summary>
+        public EmbedMessagePage()
+        {
+            this.InitializeComponent();
+
+            var applicationWidth = 550;
+            var applicationHeight = 720;
+
+            ApplicationView.PreferredLaunchViewSize = new Size
+                { Width = applicationWidth, Height = applicationHeight };
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+            ApplicationView.GetForCurrentView()
+                .SetPreferredMinSize(new Size(applicationWidth, applicationHeight));
+
+            this.modifiedImage = null;
+            this.dpiX = 0;
+            this.dpiY = 0;
+
+            this.fileWriter = new FileWriter();
+            this.fileReader = new FileReader();
+        }
+
+        #endregion
+
+        #region Methods
+
+        private async void openButton_Click(object sender, RoutedEventArgs e)
+        {
+            var sourceImageFile = await this.fileReader.SelectSourceImageFile();
+            var copyBitmapImage = await this.MakeACopyOfTheFileToWorkOn(sourceImageFile);
+
+            using (var fileStream = await sourceImageFile.OpenAsync(FileAccessMode.Read))
+            {
+                var decoder = await BitmapDecoder.CreateAsync(fileStream);
+                var transform = new BitmapTransform
+                {
+                    ScaledWidth = Convert.ToUInt32(copyBitmapImage.PixelWidth),
+                    ScaledHeight = Convert.ToUInt32(copyBitmapImage.PixelHeight)
+                };
+
+                this.dpiX = decoder.DpiX;
+                this.dpiY = decoder.DpiY;
+
+                var pixelData = await decoder.GetPixelDataAsync(
+                    BitmapPixelFormat.Bgra8,
+                    BitmapAlphaMode.Straight,
+                    transform,
+                    ExifOrientationMode.IgnoreExifOrientation,
+                    ColorManagementMode.DoNotColorManage
+                );
+
+                var sourcePixels = pixelData.DetachPixelData();
+
+                this.giveImageRedTint(sourcePixels, decoder.PixelWidth, decoder.PixelHeight);
+
+                this.modifiedImage = new WriteableBitmap((int)decoder.PixelWidth, (int)decoder.PixelHeight);
+                using (var writeStream = this.modifiedImage.PixelBuffer.AsStream())
+                {
+                    await writeStream.WriteAsync(sourcePixels, 0, sourcePixels.Length);
+                    this.imageDisplay.Source = this.modifiedImage;
+                }
+            }
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.fileWriter.SaveWritableBitmap(this.modifiedImage, this.dpiX, this.dpiY);
+        }
+
+        private void giveImageRedTint(byte[] sourcePixels, uint imageWidth, uint imageHeight)
+        {
+            for (var i = 0; i < imageHeight; i++)
+            {
+                for (var j = 0; j < imageWidth; j++)
+                {
+                    var pixelColor = this.GetPixelBgra8(sourcePixels, i, j, imageWidth, imageHeight);
+
+                    pixelColor.R = 255;
+
+                    this.SetPixelBgra8(sourcePixels, i, j, pixelColor, imageWidth, imageHeight);
+                }
+            }
+        }
+
+        private async Task<BitmapImage> MakeACopyOfTheFileToWorkOn(StorageFile imageFile)
+        {
+            IRandomAccessStream inputStream = await imageFile.OpenReadAsync();
+            var newImage = new BitmapImage();
+            newImage.SetSource(inputStream);
+            return newImage;
+        }
+
+        /// <summary>
+        ///     Gets the pixel bgra8 color from the current pixel.
+        /// </summary>
+        /// <param name="pixels">The pixels.</param>
+        /// <param name="x">The x.</param>
+        /// <param name="y">The y.</param>
+        /// <param name="width">The width.</param>
+        /// <param name="height">The height.</param>
+        /// <returns>The color of the current pixel</returns>
+        public Color GetPixelBgra8(byte[] pixels, int x, int y, uint width, uint height)
+        {
+            var offset = (x * (int)width + y) * 4;
+            var r = pixels[offset + 2];
+            var g = pixels[offset + 1];
+            var b = pixels[offset + 0];
+            return Color.FromArgb(0, r, g, b);
+        }
+
+        /// <summary>
+        ///     Sets the pixel bgra8 color to the current pixel.
+        /// </summary>
+        /// <param name="pixels">The pixels.</param>
+        /// <param name="x">The x.</param>
+        /// <param name="y">The y.</param>
+        /// <param name="color">The color.</param>
+        /// <param name="width">The width.</param>
+        /// <param name="height">The height.</param>
+        public void SetPixelBgra8(byte[] pixels, int x, int y, Color color, uint width, uint height)
+        {
+            var offset = (x * (int)width + y) * 4;
+            pixels[offset + 2] = color.R;
+            pixels[offset + 1] = color.G;
+            pixels[offset + 0] = color.B;
+        }
+
+        #endregion
+    }
+}
--- View/MainPage.xaml.cs
+++ View/MainPage.xaml.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
 using Windows.Graphics.Imaging;
 using Windows.Storage;
 using Windows.Storage.Streams;
 using Windows.UI;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media.Imaging;
@@ -17,16 +21,6 @@ namespace GroupNStegafy.View
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        #region Data members
-
-        private double dpiX;
-        private double dpiY;
-        private WriteableBitmap modifiedImage;
-        private readonly FileWriter fileWriter;
-        private readonly FileReader fileReader;
-
-        #endregion
-
         #region Constructors
 
         /// <summary>
@@ -36,118 +30,42 @@ namespace GroupNStegafy.View
         {
             this.InitializeComponent();
 
-            this.modifiedImage = null;
-            this.dpiX = 0;
-            this.dpiY = 0;
+            var applicationWidth = 500;
+            var applicationHeight = 200;
 
-            this.fileWriter = new FileWriter();
-            this.fileReader = new FileReader();
+            ApplicationView.PreferredLaunchViewSize = new Size
+                { Width = applicationWidth, Height = applicationHeight };
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+            ApplicationView.GetForCurrentView()
+                .SetPreferredMinSize(new Size(applicationWidth, applicationHeight));
         }
 
         #endregion
 
-        #region Methods
-
-        private async void openButton_Click(object sender, RoutedEventArgs e)
+        private async void EmbedButton_Click(object sender, RoutedEventArgs e)
         {
-            var sourceImageFile = await this.fileReader.SelectSourceImageFile();
-            var copyBitmapImage = await this.MakeACopyOfTheFileToWorkOn(sourceImageFile);
-
-            using (var fileStream = await sourceImageFile.OpenAsync(FileAccessMode.Read))
-            {
-                var decoder = await BitmapDecoder.CreateAsync(fileStream);
-                var transform = new BitmapTransform {
-                    ScaledWidth = Convert.ToUInt32(copyBitmapImage.PixelWidth),
-                    ScaledHeight = Convert.ToUInt32(copyBitmapImage.PixelHeight)
-                };
-
-                this.dpiX = decoder.DpiX;
-                this.dpiY = decoder.DpiY;
-
-                var pixelData = await decoder.GetPixelDataAsync(
-                    BitmapPixelFormat.Bgra8,
-                    BitmapAlphaMode.Straight,
-                    transform,
-                    ExifOrientationMode.IgnoreExifOrientation,
-                    ColorManagementMode.DoNotColorManage
-                );
-
-                var sourcePixels = pixelData.DetachPixelData();
-
-                this.giveImageRedTint(sourcePixels, decoder.PixelWidth, decoder.PixelHeight);
-
-                this.modifiedImage = new WriteableBitmap((int) decoder.PixelWidth, (int) decoder.PixelHeight);
-                using (var writeStream = this.modifiedImage.PixelBuffer.AsStream())
+            var viewId = 0;
+            var newView = CoreApplication.CreateNewView();
+            await newView.Dispatcher.RunAsync(
+                CoreDispatcherPriority.Normal,
+                () =>
                 {
-                    await writeStream.WriteAsync(sourcePixels, 0, sourcePixels.Length);
-                    this.imageDisplay.Source = this.modifiedImage;
-                }
-            }
+                    var frame = new Frame();
+                    frame.Navigate(typeof(EmbedMessagePage), null);
+                    Window.Current.Content = frame;
+
+                    viewId = ApplicationView.GetForCurrentView().Id;
+
+                    Window.Current.Activate();
+                });
+
+            var viewShown = await ApplicationViewSwitcher.TryShowAsStandaloneAsync(viewId);
         }
 
-        private void saveButton_Click(object sender, RoutedEventArgs e)
+        private void ExtractButton_Click(object sender, RoutedEventArgs e)
         {
-            this.fileWriter.SaveWritableBitmap(this.modifiedImage, this.dpiX, this.dpiY);
+            //TODO
+            //this.Frame.Navigate(typeof(EmbedMessagePage));
         }
-
-        private void giveImageRedTint(byte[] sourcePixels, uint imageWidth, uint imageHeight)
-        {
-            for (var i = 0; i < imageHeight; i++)
-            {
-                for (var j = 0; j < imageWidth; j++)
-                {
-                    var pixelColor = this.GetPixelBgra8(sourcePixels, i, j, imageWidth, imageHeight);
-
-                    pixelColor.R = 255;
-
-                    this.SetPixelBgra8(sourcePixels, i, j, pixelColor, imageWidth, imageHeight);
-                }
-            }
-        }
-
-        private async Task<BitmapImage> MakeACopyOfTheFileToWorkOn(StorageFile imageFile)
-        {
-            IRandomAccessStream inputStream = await imageFile.OpenReadAsync();
-            var newImage = new BitmapImage();
-            newImage.SetSource(inputStream);
-            return newImage;
-        }
-
-        /// <summary>
-        ///     Gets the pixel bgra8 color from the current pixel.
-        /// </summary>
-        /// <param name="pixels">The pixels.</param>
-        /// <param name="x">The x.</param>
-        /// <param name="y">The y.</param>
-        /// <param name="width">The width.</param>
-        /// <param name="height">The height.</param>
-        /// <returns>The color of the current pixel</returns>
-        public Color GetPixelBgra8(byte[] pixels, int x, int y, uint width, uint height)
-        {
-            var offset = (x * (int) width + y) * 4;
-            var r = pixels[offset + 2];
-            var g = pixels[offset + 1];
-            var b = pixels[offset + 0];
-            return Color.FromArgb(0, r, g, b);
-        }
-
-        /// <summary>
-        ///     Sets the pixel bgra8 color to the current pixel.
-        /// </summary>
-        /// <param name="pixels">The pixels.</param>
-        /// <param name="x">The x.</param>
-        /// <param name="y">The y.</param>
-        /// <param name="color">The color.</param>
-        /// <param name="width">The width.</param>
-        /// <param name="height">The height.</param>
-        public void SetPixelBgra8(byte[] pixels, int x, int y, Color color, uint width, uint height)
-        {
-            var offset = (x * (int) width + y) * 4;
-            pixels[offset + 2] = color.R;
-            pixels[offset + 1] = color.G;
-            pixels[offset + 0] = color.B;
-        }
-
-        #endregion
     }
 }
