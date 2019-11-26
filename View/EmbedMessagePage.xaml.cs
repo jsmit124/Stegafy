8d74f1811606b83eb358071e302c27f8a6149533
jsmit124 <jsmit124@my.westga.edu>
2019-11-26 17:03:49 -0500

    more abstraction

--- View/EmbedMessagePage.xaml.cs
+++ View/EmbedMessagePage.xaml.cs
@@ -5,6 +5,7 @@ using Windows.Foundation;
 using Windows.Graphics.Imaging;
 using Windows.Storage;
 using Windows.Storage.Streams;
+using Windows.UI;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -65,6 +66,8 @@ namespace GroupNStegafy.View
 
         #region Methods
 
+        #region keep these methods in code behind
+
         private async void loadSourceButton_Click(object sender, RoutedEventArgs e)
         {
             this.sourceImageFile = await this.fileReader.SelectSourceImageFile();
@@ -97,19 +100,10 @@ namespace GroupNStegafy.View
                 this.monochromeImageDisplay.Source = bitmapImage;
             }
 
-            //TODO enable settings if source and message are loaded, not needed for demo
-
             if (this.sourceImageFile != null && this.messageImageFile != null)
             {
                 this.enableSettingsOptions();
             }
-        }
-
-        private void enableSettingsOptions()
-        {
-            this.embedButton.IsEnabled = true;
-            this.encryptionSelectionCheckBox.IsEnabled = true;
-            this.BPCCSelectionComboBox.IsEnabled = true;
         }
 
         private async void embedButton_Click(object sender, RoutedEventArgs e)
@@ -125,7 +119,7 @@ namespace GroupNStegafy.View
                 return;
             }
 
-            this.embeddedImage = new WriteableBitmap((int) sourceDecoder.PixelWidth, (int) sourceDecoder.PixelHeight);
+            this.embeddedImage = new WriteableBitmap((int)sourceDecoder.PixelWidth, (int)sourceDecoder.PixelHeight);
             using (var writeStream = this.embeddedImage.PixelBuffer.AsStream())
             {
                 await writeStream.WriteAsync(sourcePixels, 0, sourcePixels.Length);
@@ -145,7 +139,18 @@ namespace GroupNStegafy.View
             this.fileWriter.SaveWritableBitmap(this.embeddedImage, this.dpiX, this.dpiY);
         }
 
-        private async Task embedMessageInImage(byte[] sourcePixels, uint imageWidth, uint imageHeight)
+        private void enableSettingsOptions()
+        {
+            this.embedButton.IsEnabled = true;
+            this.encryptionSelectionCheckBox.IsEnabled = true;
+            this.BPCCSelectionComboBox.IsEnabled = true;
+        }
+
+        #endregion
+
+        #region methods to abstract out
+
+        private async Task embedMessageInImage(byte[] sourcePixels, uint sourceImageWidth, uint sourceImageHeight)
         {
             var messagePixels = await this.extractPixelDataFromFile(this.messageImageFile);
 
@@ -154,58 +159,27 @@ namespace GroupNStegafy.View
             var messageImageWidth = messageDecoder.PixelWidth;
             var messageImageHeight = messageDecoder.PixelHeight;
 
-            if (messageImageWidth > imageWidth || messageImageHeight > imageHeight)
+            if (messageImageWidth > sourceImageWidth || messageImageHeight > sourceImageHeight)
             {
                 await this.showMessageFileTooLargeDialog();
                 this.messageImageTooLarge = true;
                 return;
             }
 
-            for (var currY = 0; currY < imageHeight; currY++)
+            for (var currY = 0; currY < sourceImageHeight; currY++)
             {
-                for (var currX = 0; currX < imageWidth; currX++)
+                for (var currX = 0; currX < sourceImageWidth; currX++)
                 {
                     var sourcePixelColor =
-                        PixelColorInfo.GetPixelBgra8(sourcePixels, currY, currX, imageWidth, imageHeight);
+                        PixelColorInfo.GetPixelBgra8(sourcePixels, currY, currX, sourceImageWidth, sourceImageHeight);
 
-                    if (currY == 0 && currX == 0)
+                    if (currX == 0 && currY == 0)
                     {
-                        sourcePixelColor.R = 212;
-                        sourcePixelColor.G = 212;
-                        sourcePixelColor.B = 212;
+                        sourcePixelColor = this.handleFirstHeaderPixel(sourcePixelColor);
                     }
                     else if (currY == 0 && currX == 1)
                     {
-                        //handle encryption selection
-                        var isChecked = this.encryptionSelectionCheckBox.IsChecked;
-                        if (isChecked != null && (bool) isChecked)
-                        {
-                            sourcePixelColor.R |= 1; //set LSB red source pixel to 1
-                        }
-                        else
-                        {
-                            sourcePixelColor.R &= 0xfe; //set LSB red source pixel to 0
-                        }
-
-                        //handle combobox selection
-                        var selectedBPCC = (ComboBoxItem) this.BPCCSelectionComboBox.SelectedItem;
-                        if (selectedBPCC != null)
-                        {
-                            var bpcc = int.Parse(selectedBPCC.Content.ToString()); // pull selected bpcc from UI
-                            var binaryBpcc = this.calculateBinaryForBPCC(bpcc); // convert bpcc to binary representation
-                            sourcePixelColor.G =
-                                (byte) binaryBpcc; // set the green channel to binary representation of bpcc selection
-                        }
-
-                        //handle embedding type
-                        if (this.messageImageFile.FileType.Equals(".txt"))
-                        {
-                            sourcePixelColor.B |= 1; //set LSB blue source pixel to 1
-                        }
-                        else
-                        {
-                            sourcePixelColor.B &= 0xfe; //set LSB blue source pixel to 0
-                        }
+                        sourcePixelColor = this.handleSecondHeaderPixel(sourcePixelColor);
                     }
                     else
                     {
@@ -225,15 +199,77 @@ namespace GroupNStegafy.View
                             }
                         }
                     }
-
-                    PixelColorInfo.SetPixelBgra8(sourcePixels, currY, currX, sourcePixelColor, imageWidth, imageHeight);
+                    PixelColorInfo.SetPixelBgra8(sourcePixels, currY, currX, sourcePixelColor, sourceImageWidth, sourceImageHeight);
                 }
             }
         }
 
+        private Color handleFirstHeaderPixel(Color sourcePixelColor)
+        {
+            sourcePixelColor.R = 212;
+            sourcePixelColor.G = 212;
+            sourcePixelColor.B = 212;
+            return sourcePixelColor;
+        }
+
+        private Color handleSecondHeaderPixel(Color sourcePixelColor)
+        {
+            var isChecked = this.encryptionSelectionCheckBox.IsChecked;
+            sourcePixelColor = handleEncryptionSelectionHeader(isChecked, sourcePixelColor);
+
+            var selectedBPCC = (ComboBoxItem) this.BPCCSelectionComboBox.SelectedItem;
+            sourcePixelColor = this.handleBpccSelectionHeader(selectedBPCC, sourcePixelColor);
+
+            sourcePixelColor = this.handleEmbeddingTypeHeader(sourcePixelColor, this.messageImageFile);
+
+            return sourcePixelColor;
+        }
+
+        private Color handleEmbeddingTypeHeader(Color sourcePixelColor, StorageFile file)
+        {
+            if (file.FileType.Equals(".txt"))
+            {
+                sourcePixelColor.B |= 1; //set LSB blue source pixel to 1
+            }
+            else
+            {
+                sourcePixelColor.B &= 0xfe; //set LSB blue source pixel to 0
+            }
+
+            return sourcePixelColor;
+        }
+
+        private Color handleBpccSelectionHeader(ComboBoxItem selectedBPCC, Color sourcePixelColor)
+        {
+            if (selectedBPCC != null)
+            {
+                var bpcc = int.Parse(selectedBPCC.Content.ToString()); // pull selected bpcc from UI
+                var binaryBpcc = this.calculateBinaryForBPCC(bpcc); // convert bpcc to binary representation
+                sourcePixelColor.G =
+                    (byte) binaryBpcc; // set the green channel to binary representation of bpcc selection
+            }
+
+            return sourcePixelColor;
+        }
+
+        private Color handleEncryptionSelectionHeader(bool? isChecked, Color sourcePixelColor)
+        {
+            if (isChecked != null && (bool) isChecked)
+            {
+                sourcePixelColor.R |= 1; //set LSB red source pixel to 1
+            }
+            else
+            {
+                sourcePixelColor.R &= 0xfe; //set LSB red source pixel to 0
+            }
+
+            return sourcePixelColor;
+        }
+
         private async Task showMessageFileTooLargeDialog()
         {
-            var messageFileTooLargeDialog = new ContentDialog {
+            var messageFileTooLargeDialog = new ContentDialog
+            {
                 Title = "ERROR",
                 Content = "Message file exceeds the dimensions of the source image"
                           + Environment.NewLine + "Embedding will not occur"
@@ -270,7 +306,8 @@ namespace GroupNStegafy.View
             using (var fileStream = await file.OpenAsync(FileAccessMode.Read))
             {
                 var decoder = await BitmapDecoder.CreateAsync(fileStream);
-                var transform = new BitmapTransform {
+                var transform = new BitmapTransform
+                {
                     ScaledWidth = Convert.ToUInt32(copyBitmapImage.PixelWidth),
                     ScaledHeight = Convert.ToUInt32(copyBitmapImage.PixelHeight)
                 };
@@ -292,6 +329,8 @@ namespace GroupNStegafy.View
                 return pixelData.DetachPixelData();
             }
         }
+
+        #endregion
 
         #endregion
     }
